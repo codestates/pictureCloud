dbb3dc5aa93c42d8e0e12799c1cfc3d2cb02a6a7
uknwns <sillver67@gmail.com>
2022-04-04 10:03:22 +0900

    Feat :sparkles:: 커맨드 이미지 업로드 추가

--- cli/controllers/command.js
+++ cli/controllers/command.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 const { signout } = require("./signout.js");
+const { directory } = require("./directory.js");
 
 const program = new commander.Command();
 const user = program.command("signout");
@@ -45,4 +46,44 @@ user.action((cmd, args) => {
         });
     });
 });
+
+
+const user2 = program.command("image");
+user2.action((cmd, args) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "email",
+        message: "✉️  email: ",
+      },
+      {
+        type: "password",
+        name: "password",
+        message: "🔐 password: ",
+        mask: function (input) {
+          return "█" + new Array(String(input).length).join("█");
+        },
+      },
+    ])
+    .then((data) => {
+      const { email, password } = data;
+      axios
+        .post("http://localhost:4000/login", {
+          email: email,
+          password: password,
+        })
+        .then((data) => {
+          const accessToken = data.data.accessToken;
+          const message = data.data.message;
+          if (message === "ok") {
+            console.log(chalk.bgRedBright("imeage 업로드를 시작합니다."));
+            directory(email);
+          } else {
+            console.log(chalk.red("입력한 정보가 잘못되었습니다"));
+          }
+        });
+    });
+});
+
 program.parse(process.argv);
--- cli/controllers/login.js
+++ cli/controllers/login.js
@@ -8,81 +8,81 @@ const { directory } = require("./directory.js");
 const { imageList } = require("./imageList.js");
 
 module.exports = {
-  login: () => {
-    console.log(chalk.bgYellowBright.black("📲  로그인을 진행합니다."));
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "email",
-          message: "✉️  email: ",
-        },
-        {
-          type: "password",
-          name: "password",
-          message: "🔐 password: ",
-          mask: function (input) {
-            return "█" + new Array(String(input).length).join("█");
-          },
-        },
-      ])
-      .then((answers) => {
-        const { email, password } = answers;
-        axios
-          .post("http://localhost:4000/login", {
-            email: email,
-            password: password,
-          })
-          .then((data) => {
-            const accessToken = data.data.accessToken;
-            const { email, password } = JSON.parse(data.config.data);
-            if (!accessToken) {
-              console.log(chalk.red("입력한 정보가 잘못되었습니다"));
-            } else if (accessToken) {
-              console.clear();
-              console.log(chalk.bgGreen.black("✔️ 로그인 성공"));
-              inquirer
-                .prompt([
-                  {
-                    type: "list",
-                    name: "choice",
-                    message: "✅ 선택해 주세요",
-                    choices: [
-                      chalk.green("이미지 업로드"),
-                      chalk.white("이미지 목록"),
-                      chalk.blue("로그아웃"),
-                      chalk.yellow("비밀번호 변경"),
-                      chalk.red("회원탈퇴"),
-                    ],
-                  },
-                ])
-                .then((data) => {
-                  const { choice } = data;
-                  if (choice === chalk.red("회원탈퇴")) {
-                    console.clear();
-                    console.log(chalk.bgRedBright("🔚 회원탈퇴를 진행합니다"));
-                    signout(accessToken, email);
-                  } else if (choice === chalk.blue("로그아웃")) {
-                    console.clear();
-                    console.log(chalk.bgGreen.black("✔️ 로그아웃 성공"));
-                  } else if (choice === chalk.green("이미지 업로드")) {
-                    console.clear();
-                    console.log("imeage 업로드를 시작합니다.");
-                    directory(email);
-                  } else if (choice === chalk.yellow("비밀번호 변경")) {
-                    console.clear();
-                    console.log(
-                      chalk.bgMagenta.black("🔏 비밀번호 변경을 시작합니다.")
-                    );
-                    changePassword(accessToken);
-                  } else if (choice === chalk.white("이미지 목록")) {
-                    console.clear();
-                    console.log("imeage 목록입니다.");
-                    imageList(email);
-                  }
-                });
-            }
-          });
-      });
-  },
+    login: () => {
+        console.log(chalk.bgYellowBright.black("📲  로그인을 진행합니다."));
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "email",
+                    message: "✉️  email: ",
+                },
+                {
+                    type: "password",
+                    name: "password",
+                    message: "🔐 password: ",
+                    mask: function (input) {
+                        return "█" + new Array(String(input).length).join("█");
+                    },
+                },
+            ])
+            .then((answers) => {
+                const { email, password } = answers;
+                axios
+                    .post("http://localhost:4000/login", {
+                        email: email,
+                        password: password,
+                    })
+                    .then((data) => {
+                        const accessToken = data.data.accessToken;
+                        const { email, password } = JSON.parse(data.config.data);
+                        if (!accessToken) {
+                            console.log(chalk.red("입력한 정보가 잘못되었습니다"));
+                        } else if (accessToken) {
+                            console.clear();
+                            console.log(chalk.bgGreen.black("✔️ 로그인 성공"));
+                            inquirer
+                                .prompt([
+                                    {
+                                        type: "list",
+                                        name: "choice",
+                                        message: "✅ 선택해 주세요",
+                                        choices: [
+                                            chalk.green("이미지 업로드"),
+                                            chalk.white("이미지 목록"),
+                                            chalk.blue("로그아웃"),
+                                            chalk.yellow("비밀번호 변경"),
+                                            chalk.red("회원탈퇴"),
+                                        ],
+                                    },
+                                ])
+                                .then((data) => {
+                                    const { choice } = data;
+                                    if (choice === chalk.red("회원탈퇴")) {
+                                        console.clear();
+                                        console.log(chalk.bgRedBright("🔚 회원탈퇴를 진행합니다"));
+                                        signout(accessToken, email);
+                                    } else if (choice === chalk.blue("로그아웃")) {
+                                        console.clear();
+                                        console.log(chalk.bgGreen.black("✔️ 로그아웃 성공"));
+                                    } else if (choice === chalk.green("이미지 업로드")) {
+                                        console.clear();
+                                        console.log("imeage 업로드를 시작합니다.");
+                                        directory(email);
+                                    } else if (choice === chalk.yellow("비밀번호 변경")) {
+                                        console.clear();
+                                        console.log(
+                                            chalk.bgMagenta.black("🔏 비밀번호 변경을 시작합니다.")
+                                        );
+                                        changePassword(accessToken);
+                                    } else if (choice === chalk.white("이미지 목록")) {
+                                        console.clear();
+                                        console.log("imeage 목록입니다.");
+                                        imageList(email);
+                                    }
+                                });
+                        }
+                    });
+            });
+    },
 };
